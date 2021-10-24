cbb381a8c8ae68d513a35fd644dbd2634f248a80
Alexey Sapr <alexeysapr@gmail.com>
2021-10-24 18:06:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,9 +13,11 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const refs = {
   searchForm: document.querySelector('#search-form'),
   galleryList: document.querySelector('ul.gallery'),
+  loadMore: document.querySelector('button.load-more'),
 };
 
 refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMore.addEventListener('click', onLoadMore);
 
 function onSearch(event) {
   event.preventDefault();
@@ -36,6 +38,31 @@ function onSearch(event) {
       Notify.success(`Hooray! We found ${data.totalHits} images.`);
       const markup = data.hits.map(photoCardTpl).join('');
       refs.galleryList.innerHTML = markup;
+      new SimpleLightbox('.gallery .photo-card__link');
+    });
+}
+
+function onLoadMore(event) {
+  event.preventDefault();
+
+  const query = 'dog';
+
+  const key = '24000530-001c810234599b9b4d7fbf89c';
+
+  fetch(
+    `https://pixabay.com/api/?key=${key}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=20`,
+  )
+    .then(response => response.json())
+    .then(data => {
+      if (data.hits.length === 0) {
+        Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        return;
+      }
+
+      Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      const markup = data.hits.map(photoCardTpl).join('');
+      refs.galleryList.insertAdjacentHTML('beforeend', markup);
+
       new SimpleLightbox('.gallery .photo-card__link');
     });
 }
