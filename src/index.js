a606566da53ea165274d3d495eb9a6467e72a946
Alexey Sapr <alexeysapr@gmail.com>
2021-10-24 17:40:11 +0300

    Ass listener

--- src/index.js
+++ src/index.js
@@ -7,22 +7,38 @@ import photoCardTpl from './templates/gallery-item.hbs';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+// Notiflix.
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const refs = {
   searchForm: document.querySelector('#search-form'),
   galleryList: document.querySelector('ul.gallery'),
 };
 
-const key = '24000530-001c810234599b9b4d7fbf89c';
+refs.searchForm.addEventListener('submit', onSearch);
 
-fetch(
-  `https://pixabay.com/api/?key=${key}&q=cat&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=20`,
-)
-  .then(response => response.json())
-  .then(data => {
-    const markup = data.hits.map(photoCardTpl).join('');
-    refs.galleryList.innerHTML = markup;
-    new SimpleLightbox('.gallery .photo-card__link');
-  });
+function onSearch(event) {
+  event.preventDefault();
+  const query = event.currentTarget.elements.searchQuery.value;
+
+  const key = '24000530-001c810234599b9b4d7fbf89c';
+
+  fetch(
+    `https://pixabay.com/api/?key=${key}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=20`,
+  )
+    .then(response => response.json())
+    .then(data => {
+      if (data.hits.length === 0) {
+        Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        return;
+      }
+
+      Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      const markup = data.hits.map(photoCardTpl).join('');
+      refs.galleryList.innerHTML = markup;
+      new SimpleLightbox('.gallery .photo-card__link');
+    });
+}
 
 // function renderPhotoCard(photo) {
 //   const markup = photoCardTpl(photo);
