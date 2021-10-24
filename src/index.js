0b6f6026c3f11718f440290ad9f6e15d179747f5
Alexey Sapr <alexeysapr@gmail.com>
2021-10-24 16:56:45 +0300

    Add styles for pictures

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './sass/main.scss';
+
+//Template
+import photoCardTpl from './templates/gallery-item.hbs';
+
+// SimpleLightbox
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  galleryList: document.querySelector('ul.gallery'),
+};
+
+const key = '24000530-001c810234599b9b4d7fbf89c';
+
+fetch(
+  `https://pixabay.com/api/?key=${key}&q=cat&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=20`,
+)
+  .then(response => response.json())
+  .then(data => {
+    const markup = data.hits.map(photoCardTpl).join('');
+    refs.galleryList.innerHTML = markup;
+    new SimpleLightbox('.gallery .photo-card__link');
+  });
+
+// function renderPhotoCard(photo) {
+//   const markup = photoCardTpl(photo);
+//   refs.galleryList.insertAdjacentHTML('beforeend', markup);
+// }
