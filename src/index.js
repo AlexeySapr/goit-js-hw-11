8c3e96c99f90486726935feaad891dbd982eeb61
Alexey Sapr <alexeysapr@gmail.com>
2021-10-25 11:07:57 +0300

    Add SearchAPI class

--- src/index.js
+++ src/index.js
@@ -10,36 +10,36 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 // Notiflix.
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+// MyJS
+import SearchAPI from './js/searchAPI';
+
+const apiService = new SearchAPI();
+
+// Refs
 const refs = {
   searchForm: document.querySelector('#search-form'),
   galleryList: document.querySelector('ul.gallery'),
-  loadMore: document.querySelector('button.load-more'),
+  loadMoreBtn: document.querySelector('button.load-more'),
 };
 
 refs.searchForm.addEventListener('submit', onSearch);
-refs.loadMore.addEventListener('click', onLoadMore);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 function onSearch(event) {
   event.preventDefault();
-  const query = event.currentTarget.elements.searchQuery.value;
 
-  const key = '24000530-001c810234599b9b4d7fbf89c';
+  apiService.searchQuery = event.currentTarget.elements.searchQuery.value;
+  apiService.fetchSearch().then(data => {
+    if (data.hits.length === 0) {
+      Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      return;
+    }
 
-  fetch(
-    `https://pixabay.com/api/?key=${key}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=20`,
-  )
-    .then(response => response.json())
-    .then(data => {
-      if (data.hits.length === 0) {
-        Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-        return;
-      }
-
-      Notify.success(`Hooray! We found ${data.totalHits} images.`);
-      const markup = data.hits.map(photoCardTpl).join('');
-      refs.galleryList.innerHTML = markup;
-      new SimpleLightbox('.gallery .photo-card__link');
-    });
+    Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    const markup = data.hits.map(photoCardTpl).join('');
+    refs.galleryList.innerHTML = markup;
+    new SimpleLightbox('.gallery .photo-card__link');
+  });
 }
 
 function onLoadMore(event) {
---
+++ src/js/searchAPI.js
@@ -0,0 +1,33 @@
+export default class SearchAPI {
+  #baseUrl = 'https://pixabay.com/api/';
+  #privatKey = '24000530-001c810234599b9b4d7fbf89c';
+  #options = {
+    imageType: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+  #page = 1;
+  #perPage = 20;
+
+  constructor() {
+    this.searchQuery = '';
+  }
+
+  fetchSearch() {
+    return fetch(
+      `${this.#baseUrl}?key=${this.#privatKey}&q=${this.searchQuery}&image_type=${
+        this.#options.imageType
+      }&orientation=${this.#options.orientation}&safesearch=${this.#options.safesearch}&page=${
+        this.#page
+      }&per_page=${this.#perPage}`,
+    ).then(response => response.json());
+  }
+
+  //   get query() {
+  //     return this.searchQuery;
+  //   }
+
+  //   set query(newQuery) {
+  //     this.searchQuery = newQuery;
+  //   }
+}
